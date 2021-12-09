5bd687f5d7aede711098d49c38b6200be2b0209d
micjef <michaeljeffrysetiawan@gmail.com>
2021-12-09 16:13:24 +0700

    create home screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/images/medium-logo.png'
 import { UserIcon, PlusIcon, CogIcon } from '@heroicons/react/solid'
 import '../styles/styles.css'
 
 const Navbar = () => {
+  const [login, setLogin] = useState(false)
+
   return (
     <div className="navbar">
       <img src={Logo} alt="Logo" className='image' />
       <div className="right">
-        <PlusIcon className='icon' />
-        <UserIcon className='icon' />
-        <CogIcon className='icon' />
+        {
+          login === false ? (
+            <div className='loginButton'>
+              <p>Login</p>
+            </div>
+          ) : (
+            <>
+              <PlusIcon className='icon' />
+              <UserIcon className='icon' />
+              <CogIcon className='icon' />
+            </>
+          )
+        }
       </div>
     </div>
   )
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import BlogCard from '../components/BlogCard'
+import Navbar from '../components/Navbar'
 
 const HomeScreen = () => {
   return (
     <div className="home">
-      <BlogCard 
-        description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
-      />
+      <Navbar />
+      <div className="container">
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+        <BlogCard 
+          description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quaerat quas, esse, odio ad libero corporis laudantium eveniet nemo nihil, molestiae voluptatum eum vero? Deserunt quasi odit quis magni voluptate.'
+        />
+      </div>
     </div>
   )
 }
